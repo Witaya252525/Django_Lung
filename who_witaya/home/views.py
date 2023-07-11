c6a046f350ac3919622c790c66cc02b9b7480093
Witaya252525 <witaya.c@kkumail.com>
2023-07-12 00:33:51 +0700

    Complete PhaseI

--- who_witaya/home/views.py
+++ who_witaya/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import ContactMessage
 
 # Create your views here.
 
@@ -9,7 +10,31 @@ def SERVICE(request):
     return render(request ,'home/service.html')
 
 def CONTACT(request):
-    return render(request ,'home/contact.html')
+    context = {}
+    if request.method =='POST' :
+        data = request.POST.copy()
+        title = data.get('title')
+        email = data.get('email')
+        detail = data.get('detail')
+        print(data,title,email)
+        
+        if title =='' or email =='':
+             context['status'] = 'Witaya'
+             return render(request ,'home/contact.html',context)
+
+
+
+        new =ContactMessage()
+        new.title = title
+        new.email = email
+        new.detail = detail
+        new.save()
+
+        context['status'] = 'success'
+
+
+    return render(request ,'home/contact.html',context)
+
 
 
 
